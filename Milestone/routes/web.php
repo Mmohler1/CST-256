34b717052e403ae94b2914b409773c6471079c85
Mmohler1 <71154838+Mmohler1@users.noreply.github.com>
2021-02-22 16:20:52 -0700

    Add files via upload

--- Milestone/routes/web.php
+++ Milestone/routes/web.php
@@ -21,6 +21,8 @@ Route::get('/admin', 'AdminController@index')->name('admin');
 
 Route::get('/suspended', 'AdminController@suspended')->name('admin');
 
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -30,3 +32,41 @@ Route::post('/doSuspend','AdminController@trySuspend');
 Route::post('/doAdmin','AdminController@doAdmin');
 
 Route::post('/doPermSuspend','AdminController@doPermSuspend');
+
+
+//Routes for portfolio
+
+Route::get('/portfolio', 'PortfolioController@index');
+Route::get('/addPortfolio', 'PortfolioController@addPortfolio'); 
+
+Route::post('/doAdd','PortfolioController@createPortfolio');
+
+
+
+Route::post('/updateAPortfolio','PortfolioController@updatePortfolioRedirect'); //for getting to update page
+
+Route::get('/updatePortfolio', 'PortfolioController@updatePortfolio');
+
+Route::post('/doUpdate','PortfolioController@changePortfolio');
+
+Route::post('/doDelete','PortfolioController@deletePortfolio');
+
+
+//Routes for job 
+Route::get('/job', 'JobController@index');
+
+Route::get('/addJob', 'JobController@addJob');
+
+Route::post('/doAddJob','JobController@createJob');
+
+
+
+Route::post('/updateAJob','JobController@updateJobRedirect'); //for getting to update page
+
+Route::get('/updateJob', 'JobController@updateJob');
+
+Route::post('/doUpdateJob','JobController@changeJob');
+
+Route::post('/doDeleteJob','JobController@deleteJob');
+
+
